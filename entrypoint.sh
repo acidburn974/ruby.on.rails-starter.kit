fcb679fc9c772cf48a6732c5efa80e103b165ea8
Kevin B <acidburn974@users.noreply.github.com>
2020-12-02 00:13:30 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,17 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 FILE=/usr/src/project/tmp/pids/server.pid
 
-if test -f "$FILE"; then
+if [ test -f "$FILE" ]; then
     echo "Removing $FILE"
     rm -f $FILE
 fi
 
+# Makes all binaries executable
 BIN_DIR=/usr/src/project/bin
 
 if [ -d "$BIN_DIR" ]; then
-	echo "chmod +x on $BIN_DIR"
-	chmod +x $BIN_DIR/*
+    echo "chmod +x on $BIN_DIR"
+    chmod +x $BIN_DIR/*
 fi
 
 
